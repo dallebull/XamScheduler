7d80c117fffe46eb83451bb2c8caea1caf06c455
Björn Dahlberg <Bjorn.Dahlberg@yaelev.se>
2020-03-20 11:03:53 +0100

    custom entry

--- FriskaClient/AdminPage.xaml.cs
+++ FriskaClient/AdminPage.xaml.cs
@@ -13,8 +13,7 @@ namespace FriskaClient
     public partial class AdminPage : ContentPage
     {
         public string furl = App.url + "api/AdminApi/";
-        private ObservableCollection<Facit> AllAnswers { get; set; }
-        private ObservableCollection<Year> AllYears { get; set; }
+
         public int YearId { get; set; }
 
         public AdminPage()
@@ -36,24 +35,7 @@ namespace FriskaClient
         }
 
 
-        public AdminPage(int Id)
-        {
-            InitializeComponent();
-            var vm = new YearViewModel();
-            var fc = new FacitViewModel();
-
-            ToolbarItem item = new ToolbarItem
-            {
-                Text = App.User,
-
-
-            };
-
-            this.ToolbarItems.Add(item);
-            item.Clicked += OnUserDetails;
-        }
-
-
+ 
         public async void OnKontrollClicked(object sender, EventArgs args)
         {
 
